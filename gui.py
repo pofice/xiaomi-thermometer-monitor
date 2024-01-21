8c3c9dda66202ba81a27e6ec718fc10badd61b95
pofice <111642288+pofice@users.noreply.github.com>
2024-01-21 18:40:19 +0800

    Update gui.py

--- gui.py
+++ gui.py
@@ -3,6 +3,9 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Set the page layout to wide
+st.set_page_config(layout="wide")
+
 # Read the data from the JSON file
 data = []
 with open("data.json", "r") as file:
@@ -17,8 +20,13 @@ humidity = [entry["humidity"] for entry in data]
 # Create a DataFrame from the extracted data
 df = pd.DataFrame({"Time": times, "Temperature": temperatures, "Humidity": humidity})
 
-# Plot the line graph using Streamlit
-st.line_chart(df.set_index("Time"))
+# Plot the temperature line graph using Streamlit
+st.subheader("温度")
+st.line_chart(df.set_index("Time")["Temperature"])
+
+# Plot the humidity line graph using Streamlit
+st.subheader("湿度")
+st.line_chart(df.set_index("Time")["Humidity"])
 
 # Show the DataFrame as a table
 st.write(df)
